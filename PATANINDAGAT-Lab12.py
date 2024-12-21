3653ec078c97bed7abc60df65adc3ec60a164aad
rinnnnnnny.6 <maurienpatanindagat@gmail.com>
2024-12-21 15:43:13 +0800

    Update PATANINDAGAT-Lab12.py

--- PATANINDAGAT-Lab12.py
+++ PATANINDAGAT-Lab12.py
@@ -1,3 +1,4 @@
+# shows the menu
 def show_menu():
     items = {
         1: ("Burger", 5.99),
@@ -11,6 +12,8 @@ def show_menu():
         print(f"{shop}. {name} - ${cost:.2f}")
     return items
 
+
+# choose items
 def choose_item(items):
     while True:
         try:
@@ -22,6 +25,7 @@ def choose_item(items):
         except ValueError:
             print("Please input a valid number.")
 
+# payment 
 def handle_payment(amount_due):
     while True:
         try:
@@ -35,6 +39,7 @@ def handle_payment(amount_due):
         except ValueError:
             print("Enter a valid monetary value.")
 
+# shows the overall order 
 def execute_order():
     items = show_menu()
     selected_item, price = choose_item(items)
@@ -42,5 +47,6 @@ def execute_order():
     handle_payment(price)
     print("Order completed. Have a great day!")
 
+# execute the order process
 if __name__ == "__main__":
     execute_order()
